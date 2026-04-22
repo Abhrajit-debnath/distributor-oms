aceab440b0401290412d55bd662b44228337d43b
Abhrajit Debnath <debnath.abhrajit2005@gmail.com>
2026-04-22 11:36:11 +0530

    chore: setup server with express and env vars

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,25 @@
-console.log("server runnning")
+import dotenv from "dotenv"
+dotenv.config()
+import express from "express"
+
+
+const app = express()
+
+const PORT = process.env.PORT
+
+if (!PORT) {
+    throw new Error("PORT is not defined")
+}
+
+
+app.get("/health-check", (req, res) => {
+    res.json({
+        status: "active"
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on PORT ${PORT}`);
+
+})
